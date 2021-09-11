2eec16848d94f012e67359cf36a0642085d6b94b
DOUGLAS CARVALHO PEREIRA <65472072+DouglasCarvalhoPereira@users.noreply.github.com>
2021-09-11 13:08:34 -0300

    Create AULAS.py

---
+++ Python Class ---- List/AULAS.py
@@ -0,0 +1,19 @@
+list_A = [] #Declara um alista vazia
+list_B = ["Douglas", "Clayton", "Lucas", "Diego"] #Lista com nome dos irmãos
+
+type(list_A)
+
+def get_word(sentence, n):
+	# Only proceed if n is positive 
+	if n > 0:
+		words = sentence.split()
+		# Only proceed if n is not more than the number of words 
+		if n <= len(words):
+			return(words[n-1])
+	return("")
+
+print(get_word("This is a lesson about lists", 4)) # Should print: lesson
+print(get_word("This is a lesson about lists", -4)) # Nothing
+print(get_word("Now we are cooking!", 1)) # Should print: Now
+print(get_word("Now we are cooking!", 5)) # Nothing
+
