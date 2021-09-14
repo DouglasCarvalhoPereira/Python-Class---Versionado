eb64c9ec07994cd7f42acec6c0c6093ec223364f
DOUGLAS CARVALHO PEREIRA <65472072+DouglasCarvalhoPereira@users.noreply.github.com>
2021-09-13 22:42:47 -0300

    Exer 13/09

--- Python Class ---- List/EXERCÍCIO PROVA 2.py
+++ Python Class ---- List/EXERCÍCIO PROVA 2.py
@@ -6,3 +6,4 @@ def group_list(group, users):
 print(group_list("Marketing", ["Mike", "Karen", "Jake", "Tasha"])) # Should be "Marketing: Mike, Karen, Jake, Tasha"
 print(group_list("Engineering", ["Kim", "Jay", "Tom"])) # Should be "Engineering: Kim, Jay, Tom"
 print(group_list("Users", "")) # Should be "Users:"
+print(group_list("ADMIN", ['Douglas', 'Carvalho']))
