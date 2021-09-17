ea805344b781045f5d099a93d0b7a0d6a7977616
DOUGLAS CARVALHO PEREIRA <65472072+DouglasCarvalhoPereira@users.noreply.github.com>
2021-09-17 12:53:13 -0300

    Exer 17/09 12:53

--- Python Class ---- List/EXERCÍCIO PROVA 9.py
+++ Python Class ---- List/EXERCÍCIO PROVA 9.py
@@ -1,7 +1,17 @@
-while True:
-    print(x)
-    y = (x + a/x)/2
-    if y == x:
-        break
+fruti =['Banana','Abacaxi']
+letter = []
+index = 0
+while index < len(fruti):
+    letter.append(fruti[index])
+    index += 1
 
-    x = y 
+print(' '.join((letter)), 'delicia')
+
+
+for let in letter[0][-1]:
+    print(let, end='\n')
+
+iniciais = '123456'
+final = 'akg'
+for letra in iniciais:
+    print(letra + fruti[1][::-1])
