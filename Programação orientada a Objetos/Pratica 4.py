76349e89aa86cdd7f6ce33a83df7b286eaae11bf
DOUGLAS CARVALHO PEREIRA <carvalhodouglaspereira@gmail.com>
2021-09-19 11:58:27 -0300

    Study 19/09

--- Programação orientada a Objetos/Pratica 4.py
+++ Programação orientada a Objetos/Pratica 4.py
@@ -1,3 +1,13 @@
-#Métodos epeciais
+#COMPOSIÇÃO
 
+class Repository:
+    def __init__(self):
+        self.packages = {} # Recebe os pacotes # Totat variável mutável deve ir dentro do método construtor
+    def add_package(self, package):
+        self.packages[package.name] = package  #Dicionário recebe pacotes
+    def package_size(sefl):
+        reusult = 0 #Recebe e soma o tamanho dos arquivos totais
+        for package in self.package.values():
+            result += package.size
+        return result
  
---
+++ Programação orientada a Objetos/Pratica 5 - Modulos.py
@@ -0,0 +1,11 @@
+#MÓDULOS
+#Podem ser usados para organizar funções, classes
+import random
+import datetime
+
+aleatorio = random.randint(0, 1000)
+print(aleatorio)
+
+data = datetime.datetime.now()
+print(data.day) 
+print(data.year)
