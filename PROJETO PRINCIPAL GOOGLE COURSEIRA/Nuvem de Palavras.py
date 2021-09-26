fd5d36ea4cf317352397182550933de9fcebab57
DOUGLAS CARVALHO PEREIRA <65472072+DouglasCarvalhoPereira@users.noreply.github.com>
2021-09-26 15:13:23 -0300

    Atualizações

--- PROJETO PRINCIPAL GOOGLE COURSEIRA/Nuvem de Palavras.py
+++ PROJETO PRINCIPAL GOOGLE COURSEIRA/Nuvem de Palavras.py
@@ -1,9 +1,4 @@
-ref_arquivo = open("words.txt","r")
-<<<<<<< Updated upstream
-lista_de_linhas = ref_arquivo.read()
-print(lista_de_linhas)
-=======
-linhas = ref_arquivo.readline()
-for linha in linhas:
-    print(linha)
->>>>>>> Stashed changes
+ref_arquivo = open("words.txt.txt","r")
+lista_de_linhas = ref_arquivo.readlines()
+for i in lista_de_linhas:
+    print(i + "APROVADO")
