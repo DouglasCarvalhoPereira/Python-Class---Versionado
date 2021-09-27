bf3160389e0d1069c7f9aade25497f307485d576
DOUGLAS CARVALHO PEREIRA <65472072+DouglasCarvalhoPereira@users.noreply.github.com>
2021-09-26 23:55:52 -0300

    Projeto Word Cloud iniciado
    Eliminando pontuação de strings e palavras muito repetitivas e que pode não fazer sentido na nuvem de palavras!

--- PROJETO PRINCIPAL GOOGLE COURSEIRA/Nuvem de Palavras2.py
+++ PROJETO PRINCIPAL GOOGLE COURSEIRA/Nuvem de Palavras2.py
@@ -1,3 +1,18 @@
+import string
+
 ref_arquivo = open("txtNuvemPalavra.txt","r", encoding="utf-8")
-lista_de_linhas = ref_arquivo.read()
-print(lista_de_linhas)
+words_in_list = ref_arquivo.read().split( )
+pontuacao = string.punctuation
+
+new_txt = [position for position in words_in_list for letter in position if letter in pontuacao]
+
+exclude = ["do","da","você","ele","eles","como","ela","elas","nós","não","de","suas","para","que","a","o","um"
+,"uns",""]
+
+print(new_txt)
+#for position in words_in_list:
+#    for letter in position:
+#        if letter.isalpha() != False:
+#            del letter
+#        
+#        print(words_in_list)
