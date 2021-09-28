8bf1b28d770acdf22e86d2d1af6effc7334de515
DOUGLAS CARVALHO PEREIRA <65472072+DouglasCarvalhoPereira@users.noreply.github.com>
2021-09-27 23:45:52 -0300

    Importando módulo Wordcloud
    Dificuldade de importação!
    
    Problema ainda não identifciado.

--- PROJETO PRINCIPAL GOOGLE COURSEIRA/Nuvem de Palavras2.py
+++ PROJETO PRINCIPAL GOOGLE COURSEIRA/Nuvem de Palavras2.py
@@ -1,18 +1,26 @@
 import string
+import wordcloud
 
+#ABRI O ARQUIVOpip
 ref_arquivo = open("txtNuvemPalavra.txt","r", encoding="utf-8")
-words_in_list = ref_arquivo.read().split( )
-pontuacao = string.punctuation
 
-new_txt = [position for position in words_in_list for letter in position if letter in pontuacao]
+#LI O ARQUIVO .TXT
+words_in_list = ref_arquivo.read()
 
-exclude = ["do","da","você","ele","eles","como","ela","elas","nós","não","de","suas","para","que","a","o","um"
-,"uns",""]
+#PALAVRAS QUE DEVEM SER EXCLUIDAS
+exclude = ["sem","do","da","você","ele","eles","como","ela","elas","nós",
+"não","de","suas","para","que","a","o","um","uns","Não","há","é","as","os","mas","com"]
 
-print(new_txt)
-#for position in words_in_list:
-#    for letter in position:
-#        if letter.isalpha() != False:
-#            del letter
-#        
-#        print(words_in_list)
+#RETIREI A PONTUAÇÃO
+not_punctuation = ''.join([i for i in words_in_list if i not in string.punctuation]).split()
+
+#EXCLUIR PALAVRAS REPETITIVAS E SEM DEFINIÇÃO
+new_texto = ' '.join([word for word in not_punctuation if word not in exclude]).split()
+
+cloud = {}
+
+for word in new_texto:
+    cont_word = new_texto.count(word)
+    cloud[word] = cont_word
+
+print(cloud)
